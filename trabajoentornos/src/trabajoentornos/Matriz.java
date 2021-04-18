1dcf0017f8239b59442fcbaf06ce09c79bf28d32
AlessandroDomini <dominiblanchalessandro@gmail.com>
2021-04-18 19:36:27 +0200

    fillMatrixTec
    Signed-off-by: AlessandroDomini <dominiblanchalessandro@gmail.com>

--- trabajoentornos/src/trabajoentornos/Matriz.java
+++ trabajoentornos/src/trabajoentornos/Matriz.java
@@ -1,13 +1,32 @@
 package trabajoentornos;
 
+import java.util.Scanner;
+
 public class Matriz {
-	
+	public static Scanner tec = new Scanner(System.in);
+
 	private boolean matriz[][];
 	
 	public Matriz(boolean matriz[][]) {
 		this.matriz=matriz;
 	}
 	
+	
+public static void fillArrayKeyboard(int [][]m) {
+		
+		
+		for (int i = 0 ; i < m.length ; i++ ) {
+			
+	  		    for (int j = 0 ; j < m[i].length ; j++ )
+	  		    {
+	  		    	System.out.println("Write the element [" +i+ "," +j+ "]");
+	  	  			m[i][j]=tec.nextInt();
+		
+	  		    }
+		}
+	tec.nextLine();
+
+	}
 	
 
 }
