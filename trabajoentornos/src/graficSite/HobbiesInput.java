e7b9f177da09baa900c23f97f30428ffb3dd16c7
AlessandroDomini <dominiblanchalessandro@gmail.com>
2021-05-07 23:25:54 +0200

    hobbiesInput
    Signed-off-by: AlessandroDomini <dominiblanchalessandro@gmail.com>

--- trabajoentornos/src/graficSite/HobbiesInput.java
+++ trabajoentornos/src/graficSite/HobbiesInput.java
@@ -6,27 +6,64 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+import trabajoentornos.Matriz;
+
 import java.awt.GridLayout;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.SwingConstants;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Iterator;
+
 import javax.swing.JCheckBox;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class HobbiesInput extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	private JTextField textField_6;
+	private JTextField[] textFieldArray = new JTextField[7]; 
+	protected static Matriz personas= MainScreen.personas;
+	private DocumentListener textListen = new DocumentListener(){
+
+		@Override
+		public void insertUpdate(DocumentEvent e) {
+			// TODO Auto-generated method stub
+			int index=(int) e.getDocument().getProperty("index");
+			String[] compare=new String[textFieldArray.length];
+			for (int i = 0; i < compare.length; i++) {
+				compare[i]=textFieldArray[i].getText();
+			}
+			if(controlNoRepeatStringInArray(index,compare)) {
+				
+	
+			}
+			
+			
+		}
+
+		@Override
+		public void removeUpdate(DocumentEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void changedUpdate(DocumentEvent e) {
+			// TODO Auto-generated method stub
+			
+		}};
+
+	
 
 	/**
 	 * Launch the application.
@@ -43,6 +80,20 @@ public class HobbiesInput extends JFrame {
 			}
 		});
 	}
+	private boolean controlNoRepeatStringInArray(int actualperson, String[]things) {
+		boolean namerep = false;
+		int arrayLength = things.length;
+		for (int j = 0; j < arrayLength; j++) {
+			// if para comparar un nombre con otro siempre y cuando i no sea igual a j para
+			// evitar errores
+			if (things[actualperson].equals(things[j]) && actualperson != j && things[actualperson]!=null) {
+				JOptionPane.showMessageDialog(contentPane,"Sos puto","Error",JOptionPane.ERROR_MESSAGE);
+				namerep = true;
+			}
+		}
+		return namerep;
+	}
+	
 
 	/**
 	 * Create the frame.
@@ -72,40 +123,40 @@ public class HobbiesInput extends JFrame {
 		personasHolder.setBounds(150, 38, 281, 70);
 		contentPane_1.add(personasHolder);
 		
-		textField = new JTextField();
-		textField.setColumns(10);
-		textField.setBounds(204, 119, 86, 20);
-		contentPane_1.add(textField);
+		textFieldArray[0] = new JTextField();
+		textFieldArray[0].setColumns(10);
+		textFieldArray[0].setBounds(204, 119, 86, 20);
+		contentPane_1.add(textFieldArray[0]);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(204, 163, 86, 20);
-		contentPane_1.add(textField_1);
+		textFieldArray[1] = new JTextField();
+		textFieldArray[1].setColumns(10);
+		textFieldArray[1].setBounds(204, 163, 86, 20);
+		contentPane_1.add(textFieldArray[1]);
 		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(204, 213, 86, 20);
-		contentPane_1.add(textField_2);
+		textFieldArray[2] = new JTextField();
+		textFieldArray[2].setColumns(10);
+		textFieldArray[2].setBounds(204, 213, 86, 20);
+		contentPane_1.add(textFieldArray[2]);
 		
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
-		textField_3.setBounds(204, 260, 86, 20);
-		contentPane_1.add(textField_3);
+		textFieldArray[3] = new JTextField();
+		textFieldArray[3].setColumns(10);
+		textFieldArray[3].setBounds(204, 260, 86, 20);
+		contentPane_1.add(textFieldArray[3]);
 		
-		textField_4 = new JTextField();
-		textField_4.setColumns(10);
-		textField_4.setBounds(367, 119, 86, 20);
-		contentPane_1.add(textField_4);
+		textFieldArray[4] = new JTextField();
+		textFieldArray[4].setColumns(10);
+		textFieldArray[4].setBounds(367, 119, 86, 20);
+		contentPane_1.add(textFieldArray[4]);
 		
-		textField_5 = new JTextField();
-		textField_5.setColumns(10);
-		textField_5.setBounds(367, 163, 86, 20);
-		contentPane_1.add(textField_5);
+		textFieldArray[5] = new JTextField();
+		textFieldArray[5].setColumns(10);
+		textFieldArray[5].setBounds(367, 163, 86, 20);
+		contentPane_1.add(textFieldArray[5]);
 		
-		textField_6 = new JTextField();
-		textField_6.setColumns(10);
-		textField_6.setBounds(367, 213, 86, 20);
-		contentPane_1.add(textField_6);
+		textFieldArray[6] = new JTextField();
+		textFieldArray[6].setColumns(10);
+		textFieldArray[6].setBounds(367, 213, 86, 20);
+		contentPane_1.add(textFieldArray[6]);
 		
 		JLabel hobbie1Holder = new JLabel("Hobbie 1");
 		hobbie1Holder.setBounds(128, 122, 66, 14);
@@ -136,15 +187,33 @@ public class HobbiesInput extends JFrame {
 		contentPane_1.add(hobbie7Holder);
 		
 		JButton sendButtonHobbie = new JButton("Send");
-		sendButtonHobbie.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				String[] hobbies= {textField.getText(), textField_1.getText(), textField_2.getText(), 
-						textField_3.getText(), textField_4.getText(), textField_5.getText(), textField_6.getText()};
-				MainScreen.personas.setHobbies(hobbies);
+		sendButtonHobbie.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				int length=textFieldArray.length;
+				
+				String [] hobbies=new String [length];
+				
+				for (int i = 0; i <length ; i++) {
+					
+				hobbies[i]=textFieldArray[i].getText();
+					
+				}
+				dispose();
+				
+				
 			}
 		});
+	
 		sendButtonHobbie.setBounds(364, 259, 89, 23);
 		contentPane_1.add(sendButtonHobbie);
+		
+		for (int i = 0; i < textFieldArray.length; i++) {
+			
+			textFieldArray[i].getDocument().putProperty("index", i);
+			textFieldArray[i].getDocument().addDocumentListener(textListen);
+			
+		}
+		
 	}
 }
